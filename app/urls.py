c3534d09ee8e41bd65f0e90382ab1e963f7d2a77
abbeylink <abbeylink@outlook.com>
2024-04-26 19:40:21 +0100

    feat: update  files

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 
-from app.views import (IndexView, APIView,DashboardView,ObservationView,CourseView,UserView, AboutView, PricingView, PricingPlanView, ContactView)
+from app.views import (IndexView, APIView,DashboardView,ObservationView,ObservationRecordView ,CourseView,UserView, AboutView, PricingView, PricingPlanView, ContactView)
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
@@ -13,6 +13,7 @@ urlpatterns = [
     # path('get-api/', APIView.as_view(), name='get-api'),
     path('observer/dashboard/', DashboardView.as_view(), name='dashboard'),
     path('observation/', ObservationView.as_view(), name='observation'),
+    path('observation/view/', ObservationRecordView.as_view(), name='observation_data'),
     path('courses/', CourseView.as_view(), name='courses'),
     path('users/profiles/', UserView.as_view(), name='users')
 ] 
